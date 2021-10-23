bdcdc504df7b81d9f73e80712a03493ba3c95e01
Hamza <najihamzapro@gmail.com>
2021-10-23 16:02:16 +0200

    first commit

---
+++ src/main/java/com/handling/exceptHandling/ExceptHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.handling.exceptHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handling/exceptHandling/ExceptHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handling.exceptHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
